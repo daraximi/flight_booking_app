83763dcbe2cebf594fd30e50b7410fe37bf065da
Darasimi Oloyede <darasimioloyede@gmail.com>
2022-10-31 22:06:12 +0100

    Create dynamic bottom navigation bar.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flight_booking_app/Screens/bottom_bar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const BottomBar(),
     );
   }
 }
