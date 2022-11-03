4c5c78740e5eebe06725dec3c3a6ec8ba2e21da5
Darasimi Oloyede <darasimioloyede@gmail.com>
2022-11-03 21:01:28 +0100

    Create UI elements in Home Screen

--- lib/Screens/ticket_view.dart
+++ lib/Screens/ticket_view.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:flight_booking_app/utils/app_layout.dart';
 import 'package:flight_booking_app/utils/app_styles.dart';
+import 'package:flight_booking_app/widgets/thick_container.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class TicketView extends StatelessWidget {
   const TicketView({super.key});
@@ -15,25 +17,135 @@ class TicketView extends StatelessWidget {
       child: Container(
         margin: const EdgeInsets.only(left: 16),
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
+            //Showing the blue part of the card/ticket
             Container(
               decoration: BoxDecoration(
-                color: Color(0xFF526799),
-                borderRadius:  BorderRadius.only(
-                  topLeft: Radius.circular(21),
-                  topRight: Radius.circular(21))
-              ),
+                  color: Color(0xFF526799),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(21),
+                      topRight: Radius.circular(21))),
               padding: const EdgeInsets.all(16),
               child: Column(children: [
                 Row(
                   children: [
-                    Text("NYC", style: Styles.headLineStyle3.copyWith(color: Colors.white)),
-                    const Spacer(),
-                    Text("London", style:  Styles.headLineStyle3.copyWith(color: Colors.white))
-                    ],
+                    Text("NYC",
+                        style: Styles.headLineStyle3
+                            .copyWith(color: Colors.white)),
+                    Expanded(child: Container()),
+                    ThickContainer(),
+                    Expanded(
+                        child: Stack(
+                      children: [
+                        SizedBox(
+                          height: 24,
+                          child: LayoutBuilder(
+                            builder: (BuildContext context,
+                                BoxConstraints constraints) {
+                              print(
+                                  "The width is ${constraints.constrainWidth()}");
+                              return Flex(
+                                direction: Axis.horizontal,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                mainAxisSize: MainAxisSize.max,
+                                children: List.generate(
+                                    (constraints.constrainWidth() / 6).floor(),
+                                    (index) => SizedBox(
+                                          width: 3,
+                                          height: 1,
+                                          child: DecoratedBox(
+                                              decoration: BoxDecoration(
+                                                  color: Colors.white)),
+                                        )),
+                              );
+                            },
+                          ),
+                        ),
+                        Center(
+                            child: Transform.rotate(
+                                angle: 1.55,
+                                child: Icon(
+                                  Icons.local_airport_outlined,
+                                  color: Colors.white,
+                                ))),
+                      ],
+                    )),
+                    ThickContainer(),
+                    Expanded(child: Container()),
+                    Text("LDN",
+                        style:
+                            Styles.headLineStyle3.copyWith(color: Colors.white))
+                  ],
+                ),
+                const Gap(3),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                        width: 100,
+                        child: Text(
+                          "New-York",
+                          style: Styles.headLineStyle4
+                              .copyWith(color: Colors.white),
+                        )),
+                    Text("8H 30M",
+                        style: Styles.headLineStyle4
+                            .copyWith(color: Colors.white)),
+                    SizedBox(
+                        width: 100,
+                        child: Text(
+                          "London",
+                          textAlign: TextAlign.end,
+                          style: Styles.headLineStyle4
+                              .copyWith(color: Colors.white),
+                        ))
+                  ],
                 )
               ]),
-            )
+            ), //Showing the Orange part of the card
+            Container(
+                color: Styles.orangeColor,
+                child: Row(children: [
+                  const SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(10),
+                              bottomRight: Radius.circular(10))),
+                    ),
+                  ),
+                  Expanded(child: LayoutBuilder(builder:
+                      (BuildContext context, BoxConstraints constraints) {
+                    return Flex(
+                      direction: Axis.horizontal,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisSize: MainAxisSize.max,
+                      children: List.generate((constraints.constrainWidth()/15).floor(), (index) => SizedBox(
+                        width: 5,height: 1,
+                        child: DecoratedBox(
+                          decoration:BoxDecoration(
+                            color: Colors.white
+                          ) ),
+                      ))
+                    );
+                  })),
+                  const SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              bottomLeft: Radius.circular(10))),
+                    ),
+                  )
+                ])),
           ],
         ),
       ),
